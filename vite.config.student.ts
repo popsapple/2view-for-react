c5fedcec719b302dba906a7f9959cd44ca9bc7e1
Popsapple <hyeongr@visang.com>
2024-05-14 19:42:01 +0900

    sentence

--- vite.config.student.ts
+++ vite.config.student.ts
@@ -3,9 +3,29 @@ import react from '@vitejs/plugin-react'
 import * as PATH from 'path';
 
 // https://vitejs.dev/config/
+export  function myPlugin() {
+  return {
+    name: 'my-plugin',
+    async transform(code, id) {
+      // 기본적으로 헤더에 붙이는걸 없애기 위함
+      if((id + '').endsWith('css') === false) return;
+      const escapedCode = code
+      .replace('\\', '\\\\')
+      .replace(/`/g, '\\`')
+      .replace(/\$/g, '\\$');
+      return `
+        const stylesheet = new CSSStyleSheet();
+        stylesheet.replaceSync(\`${escapedCode}\`);
+        export default stylesheet;
+      `;
+    }
+  }
+}
+
+// https://vitejs.dev/config/
 export default defineConfig({
   root: PATH.resolve(process.cwd(), './src'),
-  plugins: [react()],
+  plugins: [react(), myPlugin()],
   server: {
     host: '0.0.0.0',
     port: 9002,
